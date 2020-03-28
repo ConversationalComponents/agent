4584966eb07425e47d6cf2b18b7876535af499c7
Chen Buskilla <chenb67@gmail.com>
2020-03-28 20:07:43 +0300

    use latest coco-sdk async, update deps

--- setup.py
+++ setup.py
@@ -13,13 +13,14 @@ setup(name='coco-puppet',
       version='0.0.8',
       description='Modular composable chatbot development',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Chen Buskilla',
       author_email='chen@buskilla.com',
       url='https://github.com/chenb67/puppet',
       license='GPLv3',
       install_requires=[
           "aioconsole",
-          "httpx==0.9.3"
+          "coco-sdk[async]>=0.0.6"
           ],
       extras_require={
           "discord": ["discord.py"],
